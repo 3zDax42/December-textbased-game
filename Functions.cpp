f47cce76a532486ec4e0c360db3bb4c8e9e143a0
3zDax42 <ezri@thebandit.org>
2024-12-06 15:34:16 -0700

    Create Functions.cpp

---
+++ Functions.cpp
@@ -0,0 +1,13 @@
+#include<iostream> //output library: a file of prewritten code
+#include<string> //allows the use of getline and compare
+#include<Windows.h> // allows pause, sound, and text color change
+using namespace std;
+
+void Help() {
+	cout << "Type in \"w\", \"west\", or \"go west\" to move rooms" << endl;
+	cout << "Type in \"e\", \"east\", or \"go east\" to move rooms" << endl;
+	cout << "Type in \"n\", \"north\", or \"go north\" to move rooms" << endl;
+	cout << "Type in \"s\", \"south\", or \"go south\" to move rooms" << endl;
+	cout << "Type in \"status\" to veiw your Health, Stamana and Mana" << endl;
+
+}
