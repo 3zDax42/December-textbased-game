57e8b2ef7ac6023e48451a2e08a2de696a85f657
3zDax42 <ezri@thebandit.org>
2025-01-27 13:53:20 -0700

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -2,21 +2,21 @@
 
 class Player {
 private:
-int Max_Player_Health
-int Player_Health
-int Max_Player_Stamina
-int Player_Stamina
-int Player_Mana
-int Player_Mana_Regen
+int Max_Player_Health;
+int Player_Health;
+int Max_Player_Stamina;
+int Player_Stamina;
+int Player_Mana;
+int Player_Mana_Regen;
 
 
 }
 
 class Spell {
 private:
-int Mana_Cost
-int Damage
-string Damage_Type
+int Mana_Cost;
+int Damage;
+string Damage_Type;
 public:
 void Set_Variables(int cost, int dam, string type) {Mana_Cost = cost; Damage = dam; Damage_Type = type}
 }
